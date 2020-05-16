acc82456bbb897330ac01234ad3bf4c74331f043
Norman Ruiz <norman.ruiz@icloud.com>
2020-05-16 14:32:48 -0300

    Creo solucion, agrego Modelo.

---
+++ Modelo/Articulo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Articulo
+    {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca marca { get; set; }
+        public Categoria categoria { get; set; }
+        public string Imagen { get; set; }
+        public decimal Precio { get; set; }
+        public bool Estado { get; set; }
+    }
+}
---
+++ Modelo/Categoria.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+    }
+}
